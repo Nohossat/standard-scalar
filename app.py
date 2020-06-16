46a070ec1da4d29f3847cc669e58cf5667616fe5
Nohossat TRAORE <nohossat.traore@gmail.com>
2020-06-16 17:55:50 +0200

    update readme.md file

--- app.py
+++ app.py
@@ -2,21 +2,18 @@ import pandas as pd
 import numpy as np
 from standard_scaler import StdScaler
 
-X = pd.read_csv('vine.csv')
-df_X = pd.DataFrame(X)
+df_X = pd.read_csv('vine.csv')
+X = df_X.values
+
+scaler = StdScaler()
 
 # 2 cases : 
 
-scaler = StdScaler()
-# standardize a numpy array
-scaled_X = scaler.fit_transform(X)
+# standardize a pandas Dataframe
+scaler.fit_transform(df_X)
 print(scaler.mean_, scaler.var_)
 
 
-scaler_df = StdScaler()
-# standardize a pandas Dataframe
-scaler_df.fit(df_X)
-print(scaler_df.mean_, scaler_df.var_)
-
-scaled_df = scaler_df.transform(df_X)
-
+# standardize a numpy array
+scaled_X = scaler.fit_transform(X)
+print(scaler.mean_, scaler.var_)
--- readme.md
+++ readme.md
@@ -1,3 +1,22 @@
-# Standard Scalar
+# Standard Scaler
 
-Replication du module Standard Scalar de Scikit Learn 
+A custom implementation of Standard Scaler in Python.
+
+The object Standard Scaler is based on the scikit-learn implementation.
+
+## Usage
+
+You can either standardize a Pandas Dataframe or a Numpy array.
+
+```python
+
+df_X = pd.read_csv('vine.csv')
+X = df_X.values
+
+# standardize a Pandas dataframe
+scaled_df_X = scaler.fit_transform(df_X)
+
+# standardize numpy array
+scaler = StdScaler()
+scaled_X = scaler.fit_transform(X)
+```
